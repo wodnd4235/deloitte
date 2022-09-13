8dbf1188050ded8b0de26356a1f4a48588419145
jaeuchoi <jaeuchoi@KRPC11AFA2.atrapa.deloitte.com>
2022-09-13 17:56:26 +0900

    tramsaction 설정

--- src/main/java/com/deloitte/solution/test/cotroller/TestController.java
+++ src/main/java/com/deloitte/solution/test/cotroller/TestController.java
@@ -1,4 +1,4 @@
-package com.deloitte.solution.cotroller;
+package com.deloitte.solution.test.cotroller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
