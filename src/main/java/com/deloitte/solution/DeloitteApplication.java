bd21ec5f0b1f0bb1d7464e93327444ab1208c371
jaeuchoi <jaeuchoi@KRPC11AFA2.atrapa.deloitte.com>
2022-09-15 14:26:23 +0900

    log 파일 저장 local만 우선 구현

--- src/main/java/com/deloitte/solution/DeloitteApplication.java
+++ src/main/java/com/deloitte/solution/DeloitteApplication.java
@@ -3,6 +3,7 @@ package com.deloitte.solution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
