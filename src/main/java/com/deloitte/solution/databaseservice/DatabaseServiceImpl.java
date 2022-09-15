76afd38cf249fed0b778d7fa1cbac3a2d1cd3cd9
jungukang <jungukang@KRPC1VYDK2.atrapa.deloitte.com>
2022-09-15 14:31:51 +0900

    Service Impl import 오류 수정

--- src/main/java/com/deloitte/solution/databaseservice/DatabaseServiceImpl.java
+++ src/main/java/com/deloitte/solution/databaseservice/DatabaseServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-import com.deloitte.solution.databasedao.DatabaseDAO;
 import com.deloitte.solution.databasemapper.DatabaseMapper;
 import com.deloitte.solution.databasevo.DataBaseVO;
 
