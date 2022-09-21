1a76a030590e31ead1f8657737e0ba7d04a6bd8f
daeyi <daeyi@KRPC11AF9G.atrapa.deloitte.com>
2022-09-21 14:21:54 +0900

    초기 code constant 작성

---
+++ src/main/java/com/deloitte/solution/constant/CodeConst.java
@@ -0,0 +1,60 @@
+package com.deloitte.solution.constant;
+
+public class CodeConst {
+
+	public static final String BOOLEAN_TRUE = "Y";
+
+	public static final String BOOLEAN_FALSE = "N"; 
+
+	public static final String RESULT_SUCCESS = "S";
+
+	public static final String RESULT_FAIL = "F";
+	
+	public static final String DEFAULT_DATE_PATTERN = "yyyy-MM-dd";
+	
+	public static final String DEFAULT_DATE_PATTERN_NOT_CHARACTERS = "yyyyMMdd";
+
+	public static final String DEFAULT_DATETIME_MIN_PATTERN = "yyyy-MM-dd HH:mm";
+
+	public static final String DEFAULT_DATETIME_MIN_PATTERN_NOT_CHARACTERS = "yyyyMMddHHmm";
+
+	public static final String DEFAULT_DATETIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+	public static final String DEFAULT_DATETIME_PATTERN_NOT_CHARACTERS = "yyyyMMddHHmmss";
+
+	public static final String DEFAULT_DATETIME_MILI_PATTERN = "YYYY-MM-dd HH:mm:ss.SSS";
+
+	public static final String DEFAULT_DATETIME_MILI_PATTERN_NOT_CHARACTERS = "YYYYMMddHHmmssSSS";
+
+	public static final String DEFAULT_END_DATETIME = "2999-12-31 23:59:59";
+
+	public static final String DEFAULT_CHARSET_UTF_8 = "UTF-8";
+
+	public static final String CONTENT_TYPE = "Content-Type";
+
+	public static final String PROFILE_LOCAL = "local";
+
+	public static final String PROFILE_PROD = "prod";
+
+	public static final String MASKING = "*";
+
+	public static final String CRUD_C = "C";
+
+	public static final String CRUD_R = "R";
+
+	public static final String CRUD_U = "U";
+
+	public static final String CRUD_D = "D";
+
+	public static final String PAGING_PARAM_PAGE_NUM = "pageNum";
+
+	public static final String PAGING_PARAM_ROWSPERPAGE = "rowsPerPage";
+
+	public static final String PAGING_PARAM_SORT = "sort";
+	
+	public static final String EXCEL_TYPE_XLS = "xls"; 
+	
+	public static final String EXCEL_TYPE_XLSX = "xlsx"; 
+	
+
+}
