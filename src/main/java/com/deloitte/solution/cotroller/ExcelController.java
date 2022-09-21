05c73a12f880e4a6d9b926e7ee6fb55486b9830f
daeyi <daeyi@KRPC11AF9G.atrapa.deloitte.com>
2022-09-21 14:23:15 +0900

    엑셀 업로드 및 다운로드 테스트 중

--- src/main/java/com/deloitte/solution/cotroller/ExcelController.java
+++ src/main/java/com/deloitte/solution/cotroller/ExcelController.java
@@ -2,6 +2,7 @@ package com.deloitte.solution.cotroller;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -9,17 +10,52 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.deloitte.solution.service.ExcelService;
+import com.deloitte.solution.support.BasicResponse;
+
+/**
+  * @FileName : ExcelController.java
+  * @Project : Deloitte
+  * @Date : 2022. 9. 21. 
+  * @작성자 : daeyi
+  * @변경이력 :
+  * @description : excel 다운로드 및 업로드
+  */
 @Controller
 public class ExcelController {
 	
+	@Autowired
+	ExcelService excelService;
+	
+	/**
+	  * @MethodName : excelMain
+	  * @작성일 : 2022. 9. 21.
+	  * @작성자 : daeyi
+	  * @변경이력 : 
+	  * @description : 엑셀 다운로드 화면 이동
+	  * @return
+	  */
 	@RequestMapping("/excel")
 	public String excelMain() {
 		return "excel/download";
 	}
 	
+	/**
+	  * @MethodName : excelDownload
+	  * @작성일 : 2022. 9. 21.
+	  * @작성자 : daeyi
+	  * @변경이력 : 
+	  * @description : 엑셀 다운로드 처리
+	  * @param response
+	  * @throws IOException
+	  */
 	@RequestMapping("/excel/download")
     public void excelDownload(HttpServletResponse response) throws IOException {
 //        Workbook wb = new HSSFWorkbook();
@@ -58,4 +94,21 @@ public class ExcelController {
         workbook.write(response.getOutputStream());
         workbook.close();
     }
+	
+	/**
+	  * @MethodName : addExcel
+	  * @작성일 : 2022. 9. 21.
+	  * @작성자 : daeyi
+	  * @변경이력 : 
+	  * @description : 엑셀 업로드 처리
+	  * @param request
+	  * @param response
+	  * @param file
+	  * @return
+	  */
+	@RequestMapping(value = "/addExcel", method = RequestMethod.POST)
+	public ResponseEntity<? extends BasicResponse> addExcel(HttpServletRequest request,
+			HttpServletResponse response, MultipartFile file) {
+		return ResponseEntity.ok().body(excelService.addExcel(file));
+	};
 }
--- src/main/java/com/deloitte/solution/cotroller/PopbillController.java
+++ src/main/java/com/deloitte/solution/cotroller/PopbillController.java
@@ -1,5 +1,7 @@
 package com.deloitte.solution.cotroller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.deloitte.solution.dto.PopbillDto;
